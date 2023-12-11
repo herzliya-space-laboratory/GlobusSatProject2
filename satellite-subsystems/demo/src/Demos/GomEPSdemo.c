77a881abc11bd440b9273d5980d8784bb67a130a
Maayan Carmi <maayan.carmi.mc@gmail.com>
2023-12-11 18:38:04 +0200

    change to vbatt

--- satellite-subsystems/demo/src/Demos/GomEPSdemo.c
+++ satellite-subsystems/demo/src/Demos/GomEPSdemo.c
@@ -242,11 +242,11 @@ static Boolean EPS_Status(void)
 	printf("\r\nEPS Status HK General \r\n\n");
 	print_error(GomEpsGetHkData_general(0, &myEpsStatus_hk));
 	if(myEpsStatus_hk.fields.vbatt > 7400)
-		printf("EPS status is Operational \nThe vbatt equal to %d\r\n", myEpsStatus_hk / 1000.0);
+		printf("EPS status is Operational \nThe vbatt equal to %lf\r\n", (myEpsStatus_hk.fields.vbatt / 1000.0));
 	else if(myEpsStatus_hk.fields.vbatt > 7000)
-		printf("EPS status is Cruise \nThe vbatt equal to %d\r\n", myEpsStatus_hk / 1000.0);
+		printf("EPS status is Cruise \nThe vbatt equal to %lf\r\n", (myEpsStatus_hk.fields.vbatt / 1000.0));
 	else
-		printf("The satellite is in Power Safe mode \nThe vbatt equal to %d\r\n", myEpsStatus_hk / 1000.0);
+		printf("The satellite is in Power Safe mode \nThe vbatt equal to %lf\r\n", (myEpsStatus_hk.fields.vbatt / 1000.0));
 
 	return TRUE;
 }
