35afc45a6a6a3d885f5481b2f6318e5673880649
Maayan Carmi <maayan.carmi.mc@gmail.com>
2024-04-24 18:55:20 +0300

    Update IsisSPv2demo.c
    add commants to the code

--- satellite-subsystems/demo/src/Demos/IsisSPv2demo.c
+++ satellite-subsystems/demo/src/Demos/IsisSPv2demo.c
@@ -19,6 +19,9 @@
 
 #include <stdint.h>
 
+/*
+ * gets and prints the solar panels temperature.
+ * */
 Boolean SolarPanelv2_Temperature()
 {
 	int error;
@@ -27,38 +30,40 @@ Boolean SolarPanelv2_Temperature()
 	int32_t paneltemp = 0;
 	float conv_temp;
 
-	IsisSolarPanelv2_wakeup();
+	IsisSolarPanelv2_wakeup(); //Wakes the internal temperature sensor from sleep mode.
 
 	printf("\r\n Temperature values \r\n");
 
-	for( panel = 0; panel < ISIS_SOLAR_PANEL_COUNT; panel++ )
+	for( panel = 0; panel < ISIS_SOLAR_PANEL_COUNT; panel++ )  //go for the count of solar panels we have.
 	{
-		error = IsisSolarPanelv2_getTemperature(panel, &paneltemp, &status);
-		if( error )
+		error = IsisSolarPanelv2_getTemperature(panel, &paneltemp, &status);  //gets the temperature of each panel and the error message.
+		if( error ) //if there is an error
 		{
-			printf("Panel %d : Error (%d), Status (0x%X) \r\n", panel, error, status);
+			printf("Panel %d : Error (%d), Status (0x%X) \r\n", panel, error, status); //print what panel, which error and status
 			continue;
 		}
 
 		conv_temp = (float)(paneltemp) * ISIS_SOLAR_PANEL_CONV;
 
-		printf("Panel %d : %f \n", panel, conv_temp);
+		printf("Panel %d : %f \n", panel, conv_temp); //else prints
 	}
 
-	IsisSolarPanelv2_sleep();
+	IsisSolarPanelv2_sleep(); //Puts the internal temperature sensor to sleep mode
 
-	vTaskDelay( 1 / portTICK_RATE_MS );
+	vTaskDelay( 1 / portTICK_RATE_MS ); //Delay the program
 
 	return TRUE;
 }
-
+/**
+ * get which states the polar panels in (awake, sleep, no_init)
+ * */
 Boolean SolarPanelv2_State() {
 	IsisSolarPanelv2_State_t state;
 
 	IsisSolarPanelv2_wakeup();
 
-	state = IsisSolarPanelv2_getState();
-	switch (state) {
+	state = IsisSolarPanelv2_getState(); //gets the states
+	switch (state) { //print the states
 	case ISIS_SOLAR_PANEL_STATE_NOINIT:
 		printf("the current state is: NOINIT \r\n");
 		break;
@@ -76,26 +81,35 @@ Boolean SolarPanelv2_State() {
 
 	return TRUE;
 }
+
+/*
+ * Asks the user which test he wants or if he wants to exit the test loop.
+ * all the functions returns TRUE while the exit is FALSE.
+ * @return type= Boolean; offerMoreTest that get to an infinite loop and the loop ends if the function return FALSE.
+ * */
 Boolean selectAndExecuteSolarPanelsv2DemoTest()
 {
 	int selection = 0;
 	Boolean offerMoreTests = TRUE;
 
 	printf("\n\r Select a test to perform: \n\r");
+	printf("\t 0) Return to main menu \n\r");
 	printf("\t 1) Solar internal temperature sensor State \n\r");
-	printf("\t 2) Return to main menu \n\r");
+	printf("\t 2) Get solar panels states \n\r");
 
-	while(UTIL_DbguGetIntegerMinMax(&selection, 1, 2) == 0);
+	while(UTIL_DbguGetIntegerMinMax(&selection, 0, 2) == 0); //you have to write a number between the two numbers include or else it ask you to enter a number between the two.
 
 	switch(selection)
 	{
+	case 0:
+		offerMoreTests = FALSE;
+		break;
 	case 1:
 		offerMoreTests = SolarPanelv2_Temperature();
 		break;
 	case 2:
-		offerMoreTests = FALSE;
+		offerMoreTests = SolarPanelv2_State();
 		break;
-
 	default:
 		break;
 	}
