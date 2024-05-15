96323ba67befd522f7a1616ce54c3d06df5dc306
Maayan Carmi <maayan.carmi.mc@gmail.com>
2024-05-15 18:35:26 +0300

    GetRadfatTemp - create and need check
    Demos\payload_demo.c
    the function gets the temperature of the RADFAT and print the temp as unsigned int.
    we finished and need to check on the sat.
    Boolean GetRadfatTemp() and return TRUE
    
    we also create a struct to print the temp (to change the varible)

--- satellite-subsystems/demo/src/Demos/payload_demo.c
+++ satellite-subsystems/demo/src/Demos/payload_demo.c
@@ -5,6 +5,40 @@
  *      Author: maayan
  */
 #include "payload_demo.h"
+#include <hal/Drivers/I2C.h>
+#include "hal/errors.h"
+#include "hal/Utility/util.h"
+
+typedef struct __attribute__ ((__packed__))
+{
+    unsigned int temp;
+} temp_telemetry;
+
+static Boolean GetRadfatTemp()
+{
+	I2Ctransfer stactTemp;
+	unsigned char readData[4] = {0};
+	unsigned char writeData[1] = {0x77};
+
+	temp_telemetry* telemetry = (temp_telemetry*)(&readData[0]);
+
+	stactTemp.slaveAddress = 0x55;
+	stactTemp.writeSize = 1;
+	stactTemp.writeData = writeData;
+	stactTemp.readData = readData;
+	stactTemp.readSize = 4;
+	stactTemp.writeReadDelay = 845;
+	int error = I2C_writeRead(&stactTemp);
+	if(!error)
+	{
+		printf("ERROR - %d", error);
+	}
+	else
+	{
+		printf("RADFAT temperature - %d", telemetry->temp);
+	}
+	return TRUE;
+}
 
 static Boolean selectAndExecutePayloadDemoTest(void)
 {
@@ -13,16 +47,16 @@ static Boolean selectAndExecutePayloadDemoTest(void)
 
 	printf("\n\rSelect a test to perform: \n\r");
 	printf("\t 0) Return to main menu \n\r");
-/*	printf("\t 1) Print beacon \n\r");*/
-	while(UTIL_DbguGetIntegerMinMax(&selection, 0, 2) == 0); //you have to write a number between the two numbers include or else it ask you to enter a number between the two.
+	printf("\t 1) Print RADFET temperature \n\r");
+	while(UTIL_DbguGetIntegerMinMax(&selection, 0, 1) == 0); //you have to write a number between the two numbers include or else it ask you to enter a number between the two.
 
 	switch(selection) {
 	case 0:
 		offerMoreTests = FALSE;
 		break;
-/*	case 1:
-		offerMoreTests;
-		break;*/
+	case 1:
+		offerMoreTests = GetRadfatTemp();
+		break;
 	default:
 		break;
 	}
