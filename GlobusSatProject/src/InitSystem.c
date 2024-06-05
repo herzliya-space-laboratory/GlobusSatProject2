0d2ad95b753bde05961cdbc444aa64bb33de442c
Maayan Carmi <maayan.carmi.mc@gmail.com>
2024-06-05 18:19:41 +0300

    create InitSubsystems
    start creating InitSubsystems. It have all the inits of the subsystems. also we have small changes in StartTIME.
    the InitSubsystems retrn -1 for false and 0 for true.

--- GlobusSatProject/src/InitSystem.c
+++ GlobusSatProject/src/InitSystem.c
@@ -13,6 +13,7 @@
 #include "SubSystemModules/Communication/TRXVU.h"
 #include "SubSystemModules/PowerManagment/EPS.h"
 
+#include "GlobalStandards.h"
 #include "InitSystem.h"
 #include "utils.h"
 
@@ -23,18 +24,30 @@ int StartFRAM(){
 	return logError(FRAM_start(), "FRAM");
 }
 
-//TODO: void WriteDefaultValuesToFRAM()
+//TODO: void WriteDefaultValuesToFRAM() {} // need to use FRAM_writeAndVerify or FRAM_write
 
 int StartI2C(){
 	return logError(I2C_start(I2CBusSpeed_Hz, I2CTransferTimeout), "I2C");
 }
 
-//TODO: need to be filled right.
 int StartSPI(){
 	return logError(SPI_start(bus1_spi, slave1_spi), "SPI");
 }
 
 //TODO: need to be filled right.
 int StartTIME(){
-	return logError(I2C_start(I2CBusSpeed_Hz, I2CTransferTimeout), "Time");
+	const Time time = UNIX_DATE_JAN_D1_Y2000;
+	return logError(Time_start(time, I2CTransferTimeout), "Time");
+}
+
+int InitSubsystems(){
+	if(StartFRAM())
+		return -1;
+	else if(StartI2C())
+		return -1;
+	else if(StartSPI())
+		return -1;
+	else if(StartTime())
+		return -1;
+	return 0;
 }
