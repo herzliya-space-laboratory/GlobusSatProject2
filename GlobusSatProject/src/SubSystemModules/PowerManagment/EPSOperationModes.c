d335bd9d88fd3278f0d413649ef21d4ad1e4a8d7
ben friedman <theprobot12@gmail.com>
2024-06-05 19:20:59 +0300

    added file EPSOperationModes.c
    added file EPSOperationModes.c
    is in charge of the AUC of the EPS batV state

---
+++ GlobusSatProject/src/SubSystemModules/PowerManagment/EPSOperationModes.c
@@ -0,0 +1,23 @@
+/*
+ * EPSOperationMode.c
+ *
+ *  Created on: 5 june 2024
+ *
+ */
+
+#include "EPSOperationModes.h"
+
+
+int EnterOperationalMode() {
+	printf("you are on the operationalMode");
+	return 0;
+
+}
+int EnterCruiseMode() {
+	printf("you entered cruiseMode");
+	return 0;
+}
+int EnterPowerSafeMode() {
+	printf("EnterPowerSafeMode");
+	return 0;
+}
