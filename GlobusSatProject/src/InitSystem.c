654b2db096b8a281859d5b60fcaa10933b32059b
Maayan Carmi <maayan.carmi.mc@gmail.com>
2024-06-05 16:51:41 +0300

    create both the function StartFram and open utils
    Create and write the function for the FRAM and also create a the file of utils and inside that file create the function LogError that prints the number of the error if there is any.

--- GlobusSatProject/src/InitSystem.c
+++ GlobusSatProject/src/InitSystem.c
@@ -6,5 +6,9 @@
  */
 
 #include "InitSystem.h"
+#include "utils.h"
+#include <hal/Storage/FRAM.h>
 
-
+int StartFRAM(){
+	return logError(FRAM_start(), "FRAM");
+}
---
+++ GlobusSatProject/src/utils.c
@@ -0,0 +1,17 @@
+/*
+ * utils.c
+ *
+ *  Created on: 5 6 2024
+ *      Author: maayan
+ */
+
+#include <stdio.h>
+
+int logError(int error ,char* msg)
+{
+	if(error != 0)
+	{
+		printf("%s - ERROR %d", msg, error);
+	}
+	return error;
+}
