4bc344b4f4c4dabfa6f71c7b2932d93823bebbcc
ben friedman <theprobot12@gmail.com>
2024-06-06 14:36:53 +0300

    big update to the eps
    in short, i have finished most of phase one code, i am EXTREMELY HAPPY with the prograss that i made today
    lets go on waht i did,
    added 3 new functions that is not in the EPS.h,
    int EPS_First_Conditioning()
    and int smoothen(voltage_t volt, float alpha, voltage_t *output)
    EPS_Loop
    
    EPS_First_Conditioning is a symetrical condition meant only to be used in the INIT,
    
    smoothen is the fundtion that is used to smooth out ireagular reasult,
    
    EPS_Loop
    the function you will use to run in the loop.
    
    things added from the function and their current condition
    GetBatteryVoltage - started and finished
    
    EPS_Loop started and midway
    EPS_Init started and midway
    
    this commit has not been tested thus can be unstable

--- GlobusSatProject/src/SubSystemModules/PowerManagment/EPS.c
+++ GlobusSatProject/src/SubSystemModules/PowerManagment/EPS.c
@@ -2,26 +2,72 @@
  * EPS.c
  *
  *  Created on: 5 6 2024
- *      Author: maayan
+ *      Author: ben friedamn
  */
+
+
+/*
+ *#TODO check if volt_brdsup is the right way to get volt
+ *#TODO finish EPS loop and init
+ */
+#include "satellite-subsystems/imepsv2_piu.h"
 
 #include "EPS.h"
 #include "EPSOperationModes.h"
 #include "GlobalStandards.h"
 #include "utils.h"
 
-voltage_t currentVolatage = 7000; //didnt make that function yet;
-voltage_t prevVolt;
+#define EPS_INDEX 100 //place holder
+int EPS_Init();
+int EPS_Loop();
+int GetBatteryVoltage(voltage_t *vbat);
+int EPS_Conditioning();
+int EPS_First_Conditioning(); //do this before the loop
+int GetAlpha(float *alpha);
+int RestoreDefaultAlpha();
+int smoothen(voltage_t volt, float alpha, voltage_t *output);
+
+voltage_t currentVolatage;
+
 float ALPHA = DEFAULT_ALPHA_VALUE;
+
+
+int EPS_Init() {
+	GetBatteryVoltage(&currentVolatage);
+	EPS_First_Conditioning();
+	return 0;
+}
+
+int EPS_Loop() {
+	voltage_t temp;
+	GetBatteryVoltage(&temp);
+	smoothen(temp, ALPHA, &currentVolatage);
+	EPS_Conditioning();
+	return 0;
+}
 
 int EPS_Conditioning() {
 	if (currentVolatage > 7500)
 		EnterOperationalMode();
-	else if (currentVolatage > 7400 && currentVolatage < 7100)
+	else if (currentVolatage < 7400 && currentVolatage > 7100)
 		EnterCruiseMode();
 	else if (currentVolatage < 7000)
 		EnterPowerSafeMode();
 
+	return 0;
+}
+/*
+ * there might be an edge case with EPS_Conditioning, if you use it in the reset,
+ * @brief, like EPS_Conditioning, but with a symmetric thresholds
+ * return 0;
+ */
+int EPS_First_Conditioning() {
+	if (currentVolatage > 7500)
+			EnterOperationalMode();
+		else if (currentVolatage < 7500 && currentVolatage > 7000)
+			EnterCruiseMode();
+		else if (currentVolatage < 7000)
+			EnterPowerSafeMode();
 	return 0;
 }
 int GetAlpha(float *alpha) {
@@ -30,6 +76,17 @@ int GetAlpha(float *alpha) {
 }
 int RestoreDefaultAlpha() {
 	ALPHA = DEFAULT_ALPHA_VALUE;
+	return 0;
+}
+int smoothen(voltage_t volt, float alpha, voltage_t *output) {
+	*output = currentVolatage - (alpha * (volt - currentVolatage));
+	return 0;
+}
+
+int GetBatteryVoltage(voltage_t *vbat) {
+	imepsv2_piu__gethousekeepingeng__from_t houseKeeping;
+	imepsv2_piu__gethousekeepingeng(EPS_INDEX, &houseKeeping);
+	*vbat = (voltage_t)houseKeeping.fields.volt_brdsup;
 	return 0;
 }
 
