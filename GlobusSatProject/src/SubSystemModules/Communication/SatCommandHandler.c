4901e2b9324c27c14c28f969705eed7906073932
Maayan Carmi <123329294+MaayanCarmi@users.noreply.github.com>
2024-07-03 17:44:04 +0300

    Update SatCommandHandler.c
    make the code more readable and add a if to check if we send it to the right sat

--- GlobusSatProject/src/SubSystemModules/Communication/SatCommandHandler.c
+++ GlobusSatProject/src/SubSystemModules/Communication/SatCommandHandler.c
@@ -12,50 +12,49 @@
 CMD_ERR ParseDataToCommand(unsigned char * data, sat_packet_t *cmd)
 {
 	if(data != NULL && cmd != NULL)
-	{
-		int plusPlace = 0;
-		if(memcpy(&cmd->ID, data, sizeof(cmd->ID)) == NULL)
-			return null_pointer_error;
-		plusPlace += sizeof(cmd->ID);
+		return null_pointer_error;
 
-		if(memcpy(&cmd->cmd_type, data + plusPlace, sizeof(cmd->cmd_type)) == NULL)
-			return null_pointer_error;
-		plusPlace += sizeof(cmd->cmd_type);
+	int plusPlace = 0;
+	if(memcpy(&cmd->ID, data, sizeof(cmd->ID)) == NULL)
+		return null_pointer_error;
+	plusPlace += sizeof(cmd->ID);
 
-		if(memcpy(&cmd->cmd_subtype, data + plusPlace, sizeof(cmd->cmd_subtype)) == NULL)
-			return null_pointer_error;
-		plusPlace += sizeof(cmd->cmd_subtype);
+	if(cmd->ID>>24 != CUBE_SAT_ID)
+		return invalid_sat_id;
 
-		if(memcpy(&cmd->length, data + plusPlace, sizeof(cmd->length)) == NULL)
-			return null_pointer_error;
-		plusPlace += sizeof(cmd->length);
+	if(memcpy(&cmd->cmd_type, data + plusPlace, sizeof(cmd->cmd_type)) == NULL)
+		return null_pointer_error;
+	plusPlace += sizeof(cmd->cmd_type);
 
-		if(memcpy(&cmd->data, data + plusPlace, cmd->length) == NULL)
-			return null_pointer_error;
+	if(memcpy(&cmd->cmd_subtype, data + plusPlace, sizeof(cmd->cmd_subtype)) == NULL)
+		return null_pointer_error;
+	plusPlace += sizeof(cmd->cmd_subtype);
+
+	if(memcpy(&cmd->length, data + plusPlace, sizeof(cmd->length)) == NULL)
+		return null_pointer_error;
+	plusPlace += sizeof(cmd->length);
+
+	if(memcpy(&cmd->data, data + plusPlace, cmd->length) == NULL)
+		return null_pointer_error;
 
 
-		return command_succsess;
-	}
-	return null_pointer_error;
+	return command_succsess;
 }
 
 
 CMD_ERR AssembleCommand(unsigned char *data, unsigned short data_length, char type, char subtype, unsigned int id, sat_packet_t *cmd)
 {
 	if(data != NULL && cmd != NULL)
-	{
-		cmd->ID = id;
-		cmd->cmd_type = type;
-		cmd->cmd_subtype = subtype;
-		cmd->length = data_length;
-		if(data_length <= MAX_COMMAND_DATA_LENGTH)
-		{
-			memcpy(&cmd->data, data, data_length);
-		}
-		else
-			return index_out_of_bound;
+		return null_pointer_error;
 
-		return command_succsess;
-	}
-	return null_pointer_error;
+	cmd->ID = id;
+	cmd->cmd_type = type;
+	cmd->cmd_subtype = subtype;
+	cmd->length = data_length;
+	if(data_length > MAX_COMMAND_DATA_LENGTH)
+		return index_out_of_bound;
+
+	memcpy(&cmd->data, data, data_length);
+
+	return command_succsess;
 }
