81181a2f39ff6918aaed552fdcf9f3a16021c5aa
Maayan Carmi <123329294+MaayanCarmi@users.noreply.github.com>
2024-07-08 20:25:24 +0300

    Update TRXVU.c
    start the TRX_Logic and add there what was writen in the main file

--- GlobusSatProject/src/SubSystemModules/Communication/TRXVU.c
+++ GlobusSatProject/src/SubSystemModules/Communication/TRXVU.c
@@ -5,6 +5,8 @@
  *      Author: maayan
  */
 
+#include <hal/errors.h>
+
 #include "satellite-subsystems/IsisTRXVU.h"
 #include "satellite-subsystems/IsisAntS.h"
 
@@ -12,6 +14,7 @@
 #include "SysI2CAddr.h"
 
 /*#define WE_HAVE_ANTS 0*/
+
 
 int InitTrxvuAndAnts(){
 	// Definition of I2C and TRXVU
@@ -52,7 +55,23 @@ int GetNumberOfFramesInBuffer()
 {
 	unsigned short frameCount;
 	int err = logError(IsisTrxvu_rcGetFrameCount(0, &frameCount), "TRXVU - IsisTrxvu_rcGetFrameCount");
-	if(err)
+	if(err != E_NO_SS_ERR)
 		return -1;
 	return frameCount;
+}
+
+int TRX_Logic()
+{
+	sat_packet_t cmd;
+	unsigned char rxframebuffer[SIZE_RXFRAME] = {0};
+	ISIStrxvuRxFrame rx_frame = {0,0,0, rxframebuffer};
+	int error = 0;
+	if(GetNumberOfFramesInBuffer() > 0)
+	{
+		error = logError(IsisTrxvu_rcGetCommandFrame(0, &rx_frame), "TRXVU - IsisTrxvu_rcGetCommandFrame");
+		if(error != E_NO_SS_ERR)
+			return error;
+		ParseDataToCommand(rx_frame.rx_framedata, &cmd);
+	}
+	return 0;
 }
