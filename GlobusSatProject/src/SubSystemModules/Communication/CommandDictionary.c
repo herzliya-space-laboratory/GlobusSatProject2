fb1cc44dbe1d116e5cbd07a87c9d6483e4bc0e44
Maayan Carmi <123329294+MaayanCarmi@users.noreply.github.com>
2024-07-09 21:06:57 +0300

    Create CommandDictionary.c
    create CommandDictionary.c and the trxvu_command_router function (the function used right now only to check the ActUponCommand function.

---
+++ GlobusSatProject/src/SubSystemModules/Communication/CommandDictionary.c
@@ -0,0 +1,19 @@
+/*
+ * CommandDictionary.c
+ *
+ *  Created on: 9 7 2024
+ *      Author: Maayan
+ */
+
+
+#include "CommandDictionary.h"
+
+int trxvu_command_router(sat_packet_t *cmd)
+{
+	if(cmd == NULL)
+	{
+
+	}
+	printf("hello world\r\n");
+	return 0;
+}
