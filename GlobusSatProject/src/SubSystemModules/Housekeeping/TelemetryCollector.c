6d8a934ce797288147b9b639b4284aeb2fc92da7
Maayan Carmi <maayan.carmi.mc@gmail.com>
2024-08-12 20:56:53 +0300

    Maintenance - create
    Boolean CheckExecutionTime(time_unix prev_time, time_unix period).
    create - need check to see if it's work and do tests

--- GlobusSatProject/src/SubSystemModules/Housekeeping/TelemetryCollector.c
+++ GlobusSatProject/src/SubSystemModules/Housekeeping/TelemetryCollector.c
@@ -11,10 +11,11 @@
 #include <hcc/api_fat.h>
 
 
-void GetCurrentWODTelemetry(WOD_Telemetry_t *wod)
+int GetCurrentWODTelemetry(WOD_Telemetry_t *wod)
 {
 	supervisor_housekeeping_t mySupervisor_housekeeping_hk; //create a variable that is the struct we need from supervisor
-		int error = Supervisor_getHousekeeping(&mySupervisor_housekeeping_hk, SUPERVISOR_SPI_INDEX); //gets the variables to the struct and also check error.
-		F_SPACE space; //same just to SD
-		int ret = f_getfreespace(f_getdrive(), &space); //gets the variables to the struct
+	int error = Supervisor_getHousekeeping(&mySupervisor_housekeeping_hk, SUPERVISOR_SPI_INDEX); //gets the variables to the struct and also check error.
+	F_SPACE space; //same just to SD
+	int ret = f_getfreespace(f_getdrive(), &space); //gets the variables to the struct
+	return error;
 }
---
+++ GlobusSatProject/src/SubSystemModules/Maintenance/Maintenance.c
@@ -0,0 +1,24 @@
+/*
+ * Maintenance.c
+ *
+ *  Created on: 12 8 2024
+ *      Author: maayan
+ */
+
+#include "Maintenance.h"
+#include <hal/Timing/Time.h>
+#include "utils.h"
+
+
+Boolean CheckExecutionTime(time_unix prev_time, time_unix period)
+{
+	unsigned int timeNow;
+	int error = logError(Time_getUnixEpoch(&timeNow), "Maintenance - Time_getUnixEpoch");
+	if(!error)
+		return FALSE;
+	if(timeNow - prev_time >= period)
+	{
+		return TRUE;
+	}
+	return FALSE;
+}
