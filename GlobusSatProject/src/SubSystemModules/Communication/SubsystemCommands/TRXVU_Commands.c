604262cc26e1706bd965cbf04f24c42628cc0057
Maayan Carmi <maayan.carmi.mc@gmail.com>
2024-09-03 14:11:52 +0300

    Update TRXVU_Commands.c
    add commands and one thing I need to check in the error msg that the wrong number has been written

--- GlobusSatProject/src/SubSystemModules/Communication/SubsystemCommands/TRXVU_Commands.c
+++ GlobusSatProject/src/SubSystemModules/Communication/SubsystemCommands/TRXVU_Commands.c
@@ -30,33 +30,33 @@ int CMD_SetBeacon_Interval(sat_packet_t *cmd)
 	time_unix new_interval;
 	int error;
 	memcpy(&new_interval, &cmd->data, cmd->length);
-	if(new_interval >= MAX_BEACON_INTERVAL)
+	if(new_interval >= MAX_BEACON_INTERVAL) // Check if new_interval is bigger then max and if it is compares him to max interval
 		new_interval = MAX_BEACON_INTERVAL;
-	else if(new_interval <= MIN_BEACON_INTERVAL)
+	else if(new_interval <= MIN_BEACON_INTERVAL) // Same only to min interval
 		new_interval = MIN_BEACON_INTERVAL;
-	error = logError(FRAM_write((unsigned char*)&new_interval, BEACON_INTERVAL_TIME_ADDR, BEACON_INTERVAL_TIME_SIZE), "InitTrxvu - FRAM_write");
+	error = logError(FRAM_write((unsigned char*)&new_interval, BEACON_INTERVAL_TIME_ADDR, BEACON_INTERVAL_TIME_SIZE), "InitTrxvu - FRAM_write"); // Write the new interval to FRAM
 	if(error)
 	{
 		unsigned char error_msg[] = "CMD_SetBeacon_Interval - Can't write to FRAM";
-		SendAckPacket(ACK_ERROR_MSG , cmd, error_msg, sizeof(error_msg));
+		SendAckPacket(ACK_ERROR_MSG , cmd, error_msg, sizeof(error_msg)); // Send ack error that says what written in error_msg (couldn't write to FRAM)
 		return error;
 	}
 	time_unix check;
-	error = logError(FRAM_read((unsigned char*)&check, BEACON_INTERVAL_TIME_ADDR, BEACON_INTERVAL_TIME_SIZE), "InitTrxvu - FRAM_read");
+	error = logError(FRAM_read((unsigned char*)&check, BEACON_INTERVAL_TIME_ADDR, BEACON_INTERVAL_TIME_SIZE), "InitTrxvu - FRAM_read"); // Read from FRAM in the place we wrote to for check
 	if(error)
 	{
 		unsigned char error_msg[] = "CMD_SetBeacon_Interval - Can't read from FRAM";
-		SendAckPacket(ACK_ERROR_MSG , cmd, error_msg, sizeof(error_msg));
+		SendAckPacket(ACK_ERROR_MSG , cmd, error_msg, sizeof(error_msg)); // Send ack error that says what written in error_msg (couldn't read from FRAM)
 		return error;
 	}
-	if(check != new_interval)
+	if(check != new_interval) // Check if what we wrote and what have been written is the same
 	{
-		unsigned char error_msg[] = "CMD_SetBeacon_Interval - didn't write the right number in FRAM";
-		SendAckPacket(ACK_ERROR_MSG , cmd, error_msg, sizeof(error_msg));
+		unsigned char error_msg[] = "CMD_SetBeacon_Interval - didn't write the right number in FRAM" + (unsigned char)(int)check;
+		SendAckPacket(ACK_ERROR_MSG , cmd, error_msg, sizeof(error_msg)); // Send ack error that says what written in error_msg (written the wrong number and what he is)
 		return error;
 	}
 
-	return logError(SendAckPacket(ACK_UPDATE_BEACON_INTERVAL , cmd, (unsigned char*)&new_interval, sizeof(new_interval)), "CMD_SetBeacon_Interval - SendAckPacket");
+	return logError(SendAckPacket(ACK_UPDATE_BEACON_INTERVAL , cmd, (unsigned char*)&new_interval, sizeof(new_interval)), "CMD_SetBeacon_Interval - SendAckPacket"); // // Send ack with the new_interval with subtype of ACK_UPDATE_BEACON_INTERVAL
 }
 
 
