7d06a474323e8cd0fcd182af1174ce4495031b53
tamary <150698724+tamaryadgar@users.noreply.github.com>
2024-09-19 17:52:28 +0300

    Update EPS_Commands.h
    ADD added the commands according to the command dictionary

--- GlobusSatProject/src/SubSystemModules/Communication/SubsystemCommands/EPS_Commands.h
+++ GlobusSatProject/src/SubSystemModules/Communication/SubsystemCommands/EPS_Commands.h
@@ -8,52 +8,48 @@ int CMD_UpdateThresholdVoltages(sat_packet_t *cmd);
 
 int CMD_GetThresholdVoltages(sat_packet_t *cmd);
 
-int CMD_UpdateSmoothingFactor(sat_packet_t *cmd);
+int CMD_UpdateAlpha(sat_packet_t *cmd);
 
 int CMD_RestoreDefaultAlpha(sat_packet_t *cmd);
 
 int CMD_RestoreDefaultThresholdVoltages(sat_packet_t *cmd);
 
-int CMD_GetSmoothingFactor(sat_packet_t *cmd);
+int CMD_GetAlpha(sat_packet_t *cmd);
 
-int CMD_EnterCruiseMode(sat_packet_t *cmd);
+int CMD_GetMode(sat_packet_t *cmd);
 
-int CMD_EnterOperationalMode(sat_packet_t *cmd);
+int CMD_SetMode(sat_packet_t *cmd);
 
-int CMD_EnterPowerSafeMode(sat_packet_t *cmd);
+int CMD_GetState(sat_packet_t *cmd);
 
-int CMD_GetCurrentMode(sat_packet_t *cmd);
+int CMD_ChangHeaterTemp(sat_packet_t *cmd);
 
-int CMD_EPS_NOP(sat_packet_t *cmd);
+//int CMD_EPS_NOP(sat_packet_t *cmd);
 
 int CMD_EPS_ResetWDT(sat_packet_t *cmd);
 
-int CMD_EPS_SetChannels(sat_packet_t *cmd);
+//int CMD_GetEpsParemeter(sat_packet_t *cmd);
 
-int CMD_SetChannels3V3_On(sat_packet_t *cmd);
+//int CMD_SetEpsParemeter(sat_packet_t *cmd);
 
-int CMD_SetChannels3V3_Off(sat_packet_t *cmd);
+//int CMD_ResetParameter(sat_packet_t *cmd);
 
-int CMD_SetChannels5V_On(sat_packet_t *cmd);
+//int CMD_ResetConfig(sat_packet_t *cmd);
 
-int CMD_SetChannels5V_Off(sat_packet_t *cmd);
+//int CMD_LoadConfig(sat_packet_t *cmd);
 
-int CMD_GetEpsParemeter(sat_packet_t *cmd);
+//int CMD_SaveConfig(sat_packet_t *cmd);
 
-int CMD_SetEpsParemeter(sat_packet_t *cmd);
+//int CMD_SolarPanelWake(sat_packet_t *cmd);
 
-int CMD_ResetParameter(sat_packet_t *cmd);
+//int CMD_SolarPanelSleep(sat_packet_t *cmd);
 
-int CMD_ResetConfig(sat_packet_t *cmd);
+//int CMD_GetSolarPanelState(sat_packet_t *cmd);
 
-int CMD_LoadConfig(sat_packet_t *cmd);
+int CMD_GetheaterValues(sat_packet_t *cmd);
 
-int CMD_SaveConfig(sat_packet_t *cmd);
+int CMD_SetheaterValues(sat_packet_t *cmd);
 
-int CMD_SolarPanelWake(sat_packet_t *cmd);
-
-int CMD_SolarPanelSleep(sat_packet_t *cmd);
-
-int CMD_GetSolarPanelState(sat_packet_t *cmd);
+int CMD_GetstateChangesOverTime(sat_packet_t *cmd);
 
 #endif /* EPS_COMMANDS_H_ */
