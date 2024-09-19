f68d133f09476c8ea65588ca62e03b442391640e
Maayan Carmi <maayan.carmi.mc@gmail.com>
2024-09-19 19:26:41 +0300

    add & where needed and change the errors to + sign

--- GlobusSatProject/src/SubSystemModules/Communication/AckErrors.h
+++ GlobusSatProject/src/SubSystemModules/Communication/AckErrors.h
@@ -8,15 +8,15 @@
 #ifndef ACKERRORS_H_
 #define ACKERRORS_H_
 
-#define ERROR_READ_FROM_FRAM			-1
-#define ERROR_WRITE_TO_FRAM				-2
-#define ERROR_WRITTEN_IN_FRAM_WRONG 	-3
+#define ERROR_READ_FROM_FRAM			1
+#define ERROR_WRITE_TO_FRAM				2
+#define ERROR_WRITTEN_IN_FRAM_WRONG 	3
 
-#define ERROR_WRITE_TO_I2C				-10
+#define ERROR_WRITE_TO_I2C				10
 
-#define ERROR_CANT_GET_TIME				-15
+#define ERROR_CANT_GET_TIME				15
 
-#define ERROR_WRONG_LENGTH_DATA			-17 //the packet sent wrong length of the data field in data
+#define ERROR_WRONG_LENGTH_DATA			17 //the packet sent wrong length of the data field in data
 
-#define ERROR_CANT_SET_END_TIME			-20
+#define ERROR_CANT_SET_END_TIME			20
 #endif /* ACKERRORS_H_ */
