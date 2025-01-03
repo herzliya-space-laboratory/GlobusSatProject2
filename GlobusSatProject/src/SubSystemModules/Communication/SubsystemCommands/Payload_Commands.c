3fe8eec737a475863391bb95c1aa2609a08ae86e
Maayan Carmi <maayan.carmi.mc@gmail.com>
2025-01-03 12:07:33 +0200

    Update Payload_Commands.c

--- GlobusSatProject/src/SubSystemModules/Communication/SubsystemCommands/Payload_Commands.c
+++ GlobusSatProject/src/SubSystemModules/Communication/SubsystemCommands/Payload_Commands.c
@@ -12,6 +12,11 @@ int CMD_TurnOnKillPayloadFlag(sat_packet_t *cmd)
 {
 	Boolean true = TRUE;
 	if(logError(FRAM_writeAndVerify((unsigned char*)&true, PAYLOAD_IS_DEAD_ADDR, PAYLOAD_IS_DEAD_SIZE), "CMD_TurnOnKillPayloadFlag - FRAM_writeAndVerify"))
+	{
+		unsigned char ackError = ERROR_WRITE_TO_FRAM;
+		SendAckPacket(ACK_ERROR_MSG, cmd, &ackError, sizeof(ackError));
+		return ackError;
+	}
 	logError(payloadTurnOff(), "CMD_TurnOnKillPayloadFlag - payloadTurnOff");
 	return SendAckPacket(ACK_KILL_PAYLOAD, cmd, NULL, 0);
 
@@ -21,6 +26,11 @@ int CMD_TurnOffKillPayloadFlag(sat_packet_t *cmd)
 {
 	Boolean false = FALSE;
 	if(logError(FRAM_writeAndVerify((unsigned char*)&false, PAYLOAD_IS_DEAD_ADDR, PAYLOAD_IS_DEAD_SIZE), "CMD_TurnOffKillPayloadFlag - FRAM_writeAndVerify"))
+	{
+		unsigned char ackError = ERROR_WRITE_TO_FRAM;
+		SendAckPacket(ACK_ERROR_MSG, cmd, &ackError, sizeof(ackError));
+		return ackError;
+	}
 	payloadTurnOn();
 	return SendAckPacket(ACK_TURN_ON_PAYLOAD, cmd, NULL, 0);
 }
